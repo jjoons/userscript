a5457c37a28d276ab657315adb3bb6ab7ae793b6
JJoonS <jjoons@jjoons.com>
2023-09-17 14:07:29 +0900

    ⚰️ deprecate: Deprecate Naver Auto Dark Mode
    - 네이버 홈페이지에서 기기 설정에 따른 다크 모드를 지원함에 따라 지원 종료

--- src/Naver-Auto_Dark_Mode/Naver-Auto_Dark_Mode.user.ts
+++ src/Naver-Auto_Dark_Mode/Naver-Auto_Dark_Mode.user.ts
@@ -3,7 +3,7 @@
 // @description 브라우저의 다크 모드 설정에 따라서 자동으로 다크 모드로 전환합니다.
 // @namespace   https://github.com/jjoons
 // @author      JS Lee
-// @version     0.1.2
+// @version     0.1.3
 // @license     MIT
 // @homepage    https://github.com/jjoons/userscript
 // @icon        https://icons.duckduckgo.com/ip3/www.naver.com.ico
@@ -14,29 +14,15 @@
 // ==/UserScript==
 
 void (function (D, L) {
-  const darkMode = matchMedia('(prefers-color-scheme: dark)')
-  const { dark } = D.documentElement.dataset as HtmlDataset
-
-  if (dark) {
-    const matchMode = () => (darkMode.matches && dark === 'true') || (!darkMode.matches && dark === 'false')
-
-    const switchMode = (mode?: boolean) => {
-      const nDarkMode: boolean | null = mode ?? matchMode() ? null : darkMode.matches
-
-      if (typeof nDarkMode === 'boolean') {
-        D.cookie = `NDARK=${nDarkMode ? 'Y' : 'N'}; domain=naver.com; max-age=31536000` // 1 Year
-        L.reload()
-      }
-    }
-
-    darkMode.addEventListener('change', function () {
-      switchMode()
-    })
-
-    switchMode()
-  }
+  alert(
+    '네이버 홈페이지에서 기기 설정에 따른 다크 모드를 지원하게 되어\n' +
+      '더 이상 [' +
+      GM_info.script.name +
+      '] 유저스크립트를 이용할 필요가 없어졌습니다.\n\n' +
+      '확장 프로그램에서 스크립트를 제거하면\n' +
+      '이 알림창이 더 이상 나타나지 않습니다.\n\n' +
+      '그 동안 이용해 주셔서 감사합니다.'
+  )
 })(document, location)
 
-interface HtmlDataset extends DOMStringMap {
-  dark?: 'true' | 'false'
-}
+declare const GM_info: any
