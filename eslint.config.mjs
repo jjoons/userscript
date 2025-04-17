c678f8854bd1acff2111ede1e36578870128d4c1
JJoonS <jjoons@jjoons.com>
2025-04-17 19:13:26 +0900

    ⬆️ Upgrade ESLint and Prettier to latest versions

---
+++ eslint.config.mjs
@@ -0,0 +1,27 @@
+import pluginJs from '@eslint/js'
+import eslintConfigPrettier from 'eslint-config-prettier/flat'
+import globals from 'globals'
+import tsEsLint from 'typescript-eslint'
+
+/** @type {import('eslint').Linter.Config[]} */
+const eslintConfig = [
+  { files: ['src/**/*.{ts}'] },
+  { languageOptions: { globals: { ...globals.browser } } },
+  pluginJs.configs.recommended,
+  ...tsEsLint.configs.recommended,
+  eslintConfigPrettier,
+  {
+    files: ['src/**/*.ts'],
+    rules: {
+      '@typescript-eslint/no-unused-vars': 'warn',
+    },
+  },
+  {
+    files: ['src/**/*.{js,mjs,cjs}', './*.{js,mjs,cjs}'],
+    rules: {
+      'no-unused-vars': 'warn',
+    },
+  },
+]
+
+export default eslintConfig
