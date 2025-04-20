3088a04ec5f3b0397049518e3186cc12f9988e67
JJoonS <jjoons@jjoons.com>
2025-04-21 03:43:02 +0900

    👔 Add util

--- shared/userscript-utils/src/dom/find.ts
+++ shared/userscript-utils/src/dom/find.ts
@@ -15,3 +15,54 @@ export const getNodeByXPathExpression = <E extends Node = Node>(
     XPathResult.FIRST_ORDERED_NODE_TYPE,
     null,
   ).singleNodeValue as E | null
+
+/**
+ * {@linkcode XPathResult} 객체에서 {@linkcode E}를 꺼내서 `Array`에 담는 유틸 함수
+ *
+ * @param xPathResult {@linkcode XPathResult} 객체
+ * @example
+ * const xPathResult = document.evaluate(
+ *   '//*[contains(@class, "item"])',
+ *   document.body,
+ *   null,
+ *   XPathResult.ORDERED_NODE_SNAPSHOT_TYPE,
+ *   null,
+ * )
+ *
+ * // returns [div.item.entry, div.item.item_b, /* ...]
+ * xPathResultToArray(xPathResult)
+ */
+export const xPathResultToArray = <E extends Node>(
+  xPathResult: XPathResult,
+): E[] => {
+  const nodes: E[] = []
+
+  switch (xPathResult.resultType) {
+    case XPathResult.ORDERED_NODE_ITERATOR_TYPE:
+    case XPathResult.UNORDERED_NODE_ITERATOR_TYPE: {
+      let item: E | null
+
+      while ((item = xPathResult.iterateNext() as E | null) !== null) {
+        nodes.push(item)
+      }
+
+      break
+    }
+
+    case XPathResult.ORDERED_NODE_SNAPSHOT_TYPE:
+    case XPathResult.UNORDERED_NODE_SNAPSHOT_TYPE: {
+      for (let i = 0; i < xPathResult.snapshotLength; i++) {
+        const item = xPathResult.snapshotItem(i) as E
+        nodes.push(item)
+      }
+
+      break
+    }
+
+    default: {
+      throw new Error('Only support snapshot or iterator type')
+    }
+  }
+
+  return nodes
+}
