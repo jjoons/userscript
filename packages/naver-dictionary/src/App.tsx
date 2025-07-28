d6df04eed21199276bc7b403aafd27af11e57697
JJoonS <jjoons@jjoons.com>
2025-07-29 03:08:13 +0900

    👔 Some changes
    - 글로벌 스타일 로직을 `App.tsx` 쪽으로 이동
    - 페이지 이동할 때마다 `document.head`에 중복되는 Element가 추가되는데 해당 Element를 제거하는 로직 추가

--- packages/naver-dictionary/src/App.tsx
+++ packages/naver-dictionary/src/App.tsx
@@ -1,10 +1,56 @@
-import { useContext } from 'preact/hooks'
+import { GM_addStyle } from '$'
+import { useContext, useEffect } from 'preact/hooks'
+import { DOMObserver } from 'userscript-utils/dom'
 import FloatingToolbar from './components/FloatingToolbar'
 import SettingsPanel from './components/SettingsPanel'
 import { ThemeContext } from './context/ThemeProvider'
+import globalCssText from './styles/global.scss?inline'
 
 const App: preact.FunctionComponent = () => {
   const themeContext = useContext(ThemeContext)
+
+  useEffect(() => {
+    const globalStyleEl = GM_addStyle(globalCssText)
+    globalStyleEl.dataset.styleName = 'global'
+
+    const headObserver = DOMObserver.getInstance({ baseNode: document.head })
+
+    const headLinkPStaticSubscriber = headObserver.subscribe({
+      selector: 'link[rel="stylesheet"][href^="https://ssl.pstatic.net"]',
+      onAdd: () => {
+        document.head.append(globalStyleEl)
+      },
+    })
+
+    const headScriptLinkSubscriber = headObserver.subscribe<HTMLScriptElement | HTMLLinkElement>({
+      selector: 'script[src], link[rel="stylesheet"][href]',
+      onAdd: ({ node }) => {
+        /**
+         * 페이지를 이동할 때마다 `document.head`에 중복으로 추가되는
+         * Element를 제거하여 페이지 이동이 점점 느려지는 문제를 늦춤
+         */
+        if (node instanceof HTMLScriptElement) {
+          const dupArr = Array.from(
+            document.head.querySelectorAll<HTMLScriptElement>(`script[src="${node.src}"]`),
+          )
+          dupArr.slice(0, -1).forEach((e) => e.remove())
+        } else if (node instanceof HTMLLinkElement) {
+          const dupArr = Array.from(
+            document.head.querySelectorAll<HTMLLinkElement>(
+              `link[rel="stylesheet"][href="${node.href}"]`,
+            ),
+          )
+          dupArr.slice(0, -1).forEach((e) => e.remove())
+        }
+      },
+    })
+
+    return () => {
+      headLinkPStaticSubscriber.unsubscribe()
+      headScriptLinkSubscriber.unsubscribe()
+      globalStyleEl.remove()
+    }
+  }, [])
 
   return (
     <div id="us_n_dict_wrap" data-theme={themeContext.theme}>
--- packages/naver-dictionary/src/index.tsx
+++ packages/naver-dictionary/src/index.tsx
@@ -1,16 +1,11 @@
-import { GM_addStyle } from '$'
 import { render } from 'preact'
-import { DOMObserver, onBodyReady } from 'userscript-utils/dom'
+import { onBodyReady } from 'userscript-utils/dom'
 import App from './App'
 import { AppProviders } from './context'
 import { initHanja, initHanjaInfoPage, initHanjaMainPage, initHanjaSearchPage } from './pages/hanja'
-import globalCssText from './styles/global.scss?inline'
 import { hookSpaNavigation } from './utils/router'
 
 hookSpaNavigation()
-
-const globalStyleEl = GM_addStyle(globalCssText)
-globalStyleEl.dataset.styleName = 'global'
 
 const rootEl = document.createElement('div')
 rootEl.id = 'us_n_dict_root'
@@ -32,15 +27,6 @@ const bootstrap = () => {
 }
 
 const init = () => {
-  const headObserver = DOMObserver.getInstance({ baseNode: document.head })
-
-  headObserver.subscribe({
-    selector: 'link[rel="stylesheet"][href^="https://ssl.pstatic.net"]',
-    onAdd: () => {
-      document.head.append(globalStyleEl)
-    },
-  })
-
   document.body.after(rootEl)
 
   render(
